666d4edb2827d08f9d2e45f9a93564fd1296a2db
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-03 15:47:24 +0100

    firebase config update

--- website/server/firebase/firebase.ts
+++ website/server/firebase/firebase.ts
@@ -1,10 +1,12 @@
 import * as admin from 'firebase-admin'
 
-if (!admin.app) {
+const cert = process.env.FIREBASE_CERT
+
+// if (!admin.app) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault()
+    credential: admin.credential.cert(JSON.parse(cert))
   })
-}
+// }
 
 export const app = admin.app
 export const firestore = admin.firestore()
