3a6bd8e0fcb9cdbef711232e075473321af6c2bc
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-04 09:33:21 +0100

    cors support added

--- website/server/graphql/Server.ts
+++ website/server/graphql/Server.ts
@@ -1,9 +1,21 @@
+import Cors from 'micro-cors'
 import {ApolloServer} from 'apollo-server-micro'
 import {typeDefs} from '../messages/Schemas'
 import {resolvers} from '../messages/Resolvers'
 
-const apolloServer = new ApolloServer({typeDefs, resolvers})
+const cors = Cors();
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 export default (path: string) => {
-  return apolloServer.createHandler({path});
+  const handler = apolloServer.createHandler({path});
+
+  return cors((req, res) =>
+    req.method === 'OPTIONS'
+      ? res.end()
+      : handler(req, res)
+  )
 }
