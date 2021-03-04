95a5580a31d2947f18db33d008bfe9ff53692201
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-04 10:26:26 +0100

    next.js cleanup and convert to tsx

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
-
-export default MyApp
+export default App
---
+++ website/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>WebChat</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        WebChat
+      </main>
+    </div>
+  )
+}
+export default Home
