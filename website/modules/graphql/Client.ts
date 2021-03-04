410f8adf9575cb43d186b38c61bd60e9c97ab0dd
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-04 11:41:25 +0100

    gql api url fix

--- website/modules/graphql/Client.ts
+++ website/modules/graphql/Client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: '/api/graphql',
   cache: new InMemoryCache()
 });
