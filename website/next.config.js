9c78031d9ad50c2c70932a7a443e6fd412a668ae
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-04 15:45:24 +0100

    client-core old config rollback

--- website/next.config.js
+++
@@ -1,2 +0,0 @@
-const withTM = require('next-transpile-modules')(['@webchat/client-core']);
-module.exports = withTM();
--- website/pages/comments/[website].tsx
+++ website/pages/comments/[website].tsx
@@ -5,7 +5,6 @@ import {GetServerSideProps} from "next";
 import {CommentModel} from "../../modules/comments/CommentModel"
 import {CommentsView} from "../../modules/comments/components/CommentsView";
 import {createComment, getComments} from "../../modules/comments/CommentClient";
-import {Test} from "@webchat/client-core"
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const service = new CommentService()
@@ -40,8 +39,6 @@ const Website: React.FC<WebsiteProps> = (props) => {
     <div>
       <main>
         {website}
-
-        <Test/>
 
         <CommentsView comments={comments} onCommentAdd={onCommentAdd}/>
       </main>
