1d000783116ea81fc6a5c6a864db75185282a44a
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-04 09:11:03 +0100

    add message graphql endpoint

--- website/server/messages/Resolvers.ts
+++ website/server/messages/Resolvers.ts
@@ -1,4 +1,5 @@
 import {MessageService} from "./MessageService";
+import {Message} from "./Message";
 
 const service = new MessageService();
 
@@ -6,12 +7,22 @@ export const resolvers = {
   Query: {
     getMessages: async (_, args) => {
       const messages = await service.getMessages(args.websiteId, args.pageId)
-      return messages.map(m => ({
-        id: m.id,
-        text: m.text,
-        websiteId: m.websiteId,
-        pageId: m.pageId
-      }))
+      return messages.map(messageToResponse)
     }
+  },
+  Mutation: {
+    addMessage: async (_, args) => {
+      const message = await service.addMessage(args.text, args.websiteId, args.pageId)
+      return messageToResponse(message)
+    }
+  }
+}
+
+const messageToResponse = (message: Message) => {
+  return {
+    id: message.id,
+    text: message.text,
+    websiteId: message.websiteId,
+    pageId: message.pageId
   }
 }
--- website/server/messages/Schemas.ts
+++ website/server/messages/Schemas.ts
@@ -1,13 +1,18 @@
 import {gql} from "apollo-server-micro"
 
 export const typeDefs = gql`
-    type  Message {
-        id: ID
-        text: String
-        websiteId: String,
-        pageId: String
-    }
+  type  Message {
+    id: ID
+    text: String
+    websiteId: String,
+    pageId: String
+  }
 
-    type  Query {
-        getMessages(websiteId: String!, pageId: String!): [Message]
-    }`
+  type  Query {
+    getMessages(websiteId: String!, pageId: String!): [Message]
+  }
+  
+  type Mutation {
+    addMessage(text: String!, websiteId: String!, pageId: String!): Message
+  }
+`
