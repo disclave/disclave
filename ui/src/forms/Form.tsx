78d100aeb2da3c5b8ab62274c6100fae2fe8b9f2
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-09 10:08:57 +0100

    unused form componeent removed

--- ui/src/forms/Form.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-interface FormProps {
-  onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void;
-}
-
-export const Form: React.FC<FormProps> = (props) => {
-  return <form onSubmit={props.onSubmit}>{props.children}</form>;
-};
