f4f60fd863d3b90693488e2900c3fbc98be48ec8
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-09 14:58:55 +0100

    button basic styles

--- ui/src/components/button/Button.tsx
+++ ui/src/components/button/Button.tsx
@@ -2,22 +2,22 @@ import React from "react";
 
 import "./Button.css";
 
-const classes = `
-  border-transparent border
-  text-white 
-  bg-indigo-600 hover:bg-indigo-700 
-  inline-flex items-center px-4 py-2
-  rounded-md shadow-sm 
-  text-sm font-medium 
-  focus:ring-indigo-500 focus:outline-node focus:ring-2 focus:ring-offset-2
-`;
-
 export interface ButtonProps {
   onClick?: () => void;
 }
 
-export const Button: React.FC<ButtonProps> = (props) => (
-  <button className={classes} onClick={props.onClick}>
-    {props.children}
-  </button>
-);
+export const Button: React.FC<ButtonProps> = (props) => {
+  const classes = `
+    bg-gray-700
+    text-white
+    rounded
+    px-4 py-2
+    focus:outline-none
+  `;
+
+  return (
+    <button className={classes} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
