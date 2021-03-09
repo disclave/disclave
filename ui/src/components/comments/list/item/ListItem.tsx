46707fb4a9409bbd580190fe9ef03d65dc0c030f
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-09 15:14:53 +0100

    comments list item styles

--- ui/src/components/comments/list/item/ListItem.tsx
+++ ui/src/components/comments/list/item/ListItem.tsx
@@ -13,12 +13,14 @@ export const ListItem: React.VFC<ListItemProps> = ({ comment }) => {
   return (
     <div>
       <div>
-        <div>{comment.author.name}</div>
-        <div>
+        <div className="font-semibold">@{comment.author.name}</div>
+        <div className="font-light text-sm">
           <DateTimePreview iso={comment.timestamp} />
         </div>
       </div>
-      <div>{comment.text}</div>
+      <div className="mt-2">
+        <p>{comment.text}</p>
+      </div>
     </div>
   );
 };
