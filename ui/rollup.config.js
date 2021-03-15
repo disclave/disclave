356fa960c7b7621705981a147ffef6319399f002
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-15 16:00:44 +0100

    ui config changed from cra/craco to rollup

---
+++ ui/rollup.config.js
@@ -0,0 +1,38 @@
+import * as path from "path";
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import del from 'rollup-plugin-delete';
+import pkg from './package.json';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+import postcssImport from 'postcss-import';
+
+
+export default {
+  input: pkg.source,
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' }
+  ],
+  plugins: [
+    external(),
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    del({ targets: ['dist/*'] }),
+    typescript(),
+    postcss({
+      extensions: ['.css'],
+      extract: path.resolve('dist/index.css'),
+      // modules: true,
+      // minimize: true,
+      config: {
+        path: './postcss.config.js'
+      },
+      plugins: [
+        postcssImport()
+      ]
+    }),
+  ],
+  external: Object.keys(pkg.peerDependencies || {}),
+};
--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,1 +1,6 @@
+import "./index.css"
+
+export { Button } from "./components/button"
+
 export { CommentsContainer } from "./components/comments";
+
