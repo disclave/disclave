76f00ce69884ad41c9d3a082188d45e5ff4294fe
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-16 11:11:49 +0100

    unused line removeed

--- ui/rollup.config.js
+++ ui/rollup.config.js
@@ -1,39 +1,35 @@
 import * as path from "path";
-import babel from '@rollup/plugin-babel';
-import external from 'rollup-plugin-peer-deps-external';
-import del from 'rollup-plugin-delete';
-import pkg from './package.json';
-import typescript from '@rollup/plugin-typescript';
-import postcss from 'rollup-plugin-postcss';
-import postcssImport from 'postcss-import';
-
+import babel from "@rollup/plugin-babel";
+import external from "rollup-plugin-peer-deps-external";
+import del from "rollup-plugin-delete";
+import pkg from "./package.json";
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import postcssImport from "postcss-import";
 
 export default {
   input: pkg.source,
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'esm' }
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "esm" },
   ],
   plugins: [
     external(),
     babel({
       presets: ["@babel/env", "@babel/preset-react"],
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled'
+      exclude: "node_modules/**",
+      babelHelpers: "bundled",
     }),
-    del({ targets: ['dist/*'] }),
+    del({ targets: ["dist/*"] }),
     typescript(),
     postcss({
-      extensions: ['.css'],
-      extract: path.resolve('dist/index.css'),
-      // modules: true,
+      extensions: [".css"],
+      extract: path.resolve("dist/index.css"),
       minimize: true,
       config: {
-        path: './postcss.config.js'
+        path: "./postcss.config.js",
       },
-      plugins: [
-        postcssImport()
-      ]
+      plugins: [postcssImport()],
     }),
   ],
   external: Object.keys(pkg.peerDependencies || {}),
