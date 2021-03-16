e3948a4a7df7bdadf5ca5954271fdebb3a01e694
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-16 14:31:14 +0100

    next.config.js locales info added

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US'
+  },
   target: 'experimental-serverless-trace',
   env: {
     FIREBASE_CLIENT_CONFIG: process.env.FIREBASE_CLIENT_CONFIG
