002db8407dbea2f7762d38fe70ff2c887995e0fb
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-16 15:32:38 +0100

    translations extracted to separate files and typescript support added

--- ui/src/i18n/index.ts
+++ ui/src/i18n/index.ts
@@ -2,22 +2,23 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import enTranslation from "./locales/en/translation.json";
+import plTranslation from "./locales/pl/translation.json";
+
+export const resources = {
+  en: {
+    translation: enTranslation,
+  },
+  pl: {
+    translation: plTranslation,
+  },
+} as const;
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: {
-        translation: {
-          "Test translation": "This is test translation!!!",
-        },
-      },
-      pl: {
-        translation: {
-          "Test translation": "To po polsku!",
-        },
-      },
-    },
+    resources,
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
