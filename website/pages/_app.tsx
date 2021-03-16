347db9cad4f22f046554e95132fd60244a9a4604
JKrol <github@kroljakub.pl>
2021-03-16 14:39:19 +0100

    Next i18n (#27)
    * next.config.js locales info added
    
    * LocaleContext created
    
    * LocaleContext provider added to next app
    
    * using LocaleContext for date formatting
    
    Co-authored-by: Jakub Król <jakub.krol@mc2innovations.com>

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -2,10 +2,17 @@ import '../styles/globals.css';
 import { useEffect, useState } from 'react';
 import { currentUser, onAuthStateChanged } from '../modules/auth/auth';
 import { UserContext } from '../modules/auth/UserContext';
+import { LocaleContext } from '@webchat/ui';
+import { useRouter } from 'next/router';
 
 const App = ({ Component, pageProps }) => {
   const [user, setUser] = useState(undefined);
   useEffect(() => onAuthStateChanged(setUser));
+
+  const router = useRouter();
+  const localeData = {
+    locale: router.locale
+  };
 
   // // TODO: remove after testing!
   // useEffect(() => {
@@ -15,9 +22,11 @@ const App = ({ Component, pageProps }) => {
   // }, [user])
 
   return (
-    <UserContext.Provider value={user}>
-      <Component {...pageProps} />
-    </UserContext.Provider>
+    <LocaleContext.Provider value={localeData}>
+      <UserContext.Provider value={user}>
+        <Component {...pageProps} />
+      </UserContext.Provider>
+    </LocaleContext.Provider>
   );
 };
 export default App;
