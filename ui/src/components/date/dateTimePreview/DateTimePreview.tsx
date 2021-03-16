183325ad23ac1cfd78ff086a2b5c11b9776b53de
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-16 14:31:59 +0100

    using LocaleContext for date formatting

--- ui/src/components/date/dateTimePreview/DateTimePreview.tsx
+++ ui/src/components/date/dateTimePreview/DateTimePreview.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./DateTimePreview.css";
+import { LocaleContext } from "../../../context/localeContext";
 
 export interface DateTimePreviewProps {
   iso: string;
@@ -11,11 +12,14 @@ export const DateTimePreview: React.VFC<DateTimePreviewProps> = ({
   iso,
   locales,
 }) => {
+  const localeContext = useContext(LocaleContext);
+  const dateLocales = locales ?? localeContext.locale;
+
   const date = new Date(iso);
 
-  const dateTimeStr = date.toLocaleString(locales);
-  const dateStr = date.toLocaleDateString(locales);
-  const timeStr = date.toLocaleTimeString(locales, {
+  const dateTimeStr = date.toLocaleString(dateLocales);
+  const dateStr = date.toLocaleDateString(dateLocales);
+  const timeStr = date.toLocaleTimeString(dateLocales, {
     hour: "2-digit",
     minute: "2-digit",
   });
