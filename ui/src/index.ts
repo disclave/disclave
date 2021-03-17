d170693805046f44a827c207574bbf3035322ed7
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-17 13:30:28 +0100

    export login form container

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -2,4 +2,5 @@ import "./index.css";
 
 export { changeLanguage } from "./i18n";
 
+export { LoginFormContainer } from "./components/auth";
 export { CommentsContainer } from "./components/comments";
