f0569871498cf9d49c3535e2ce9eeedbbbf965ed
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-17 12:59:08 +0100

    auth login translations added and i18n resiurces refactor

--- ui/src/i18n/index.ts
+++ ui/src/i18n/index.ts
@@ -2,16 +2,11 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import enComments from "./locales/en/comments.json";
-import plComments from "./locales/pl/comments.json";
+import { en, pl } from "./locales";
 
 export const resources = {
-  en: {
-    comments: enComments,
-  },
-  pl: {
-    comments: plComments,
-  },
+  en,
+  pl,
 } as const;
 
 i18n
---
+++ ui/src/i18n/locales/en/index.ts
@@ -0,0 +1,7 @@
+import auth from "./auth.json";
+import comments from "./comments.json";
+
+export const en = {
+  auth,
+  comments,
+} as const;
