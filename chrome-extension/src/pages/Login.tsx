f84cd0fc366cdccea1b55621052fede3a46174ac
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-19 16:02:40 +0100

    login view added (without logic)

--- chrome-extension/src/pages/Login.tsx
+++ chrome-extension/src/pages/Login.tsx
@@ -1,7 +1,28 @@
 import * as React from "react";
+import { LoginFormContainer } from "@webchat/ui";
+import { login, logout } from "@webchat/client";
 
 export const loginHref = "/login";
 
 export const Login = () => {
-  return <div>Login page</div>;
+  // TODO: fetch from context
+  let userProfile = null;
+
+  const onLogin = async (email: string, password: string) => {
+    // await login(email, password);
+  };
+
+  const onLogout = async () => {
+    // await logout();
+  };
+
+  return (
+    <div>
+      <LoginFormContainer
+        onLogin={onLogin}
+        onLogout={onLogout}
+        userProfile={userProfile}
+      />
+    </div>
+  );
 };
