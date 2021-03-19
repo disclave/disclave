a90c41c905916098737196448ec00ad9e4d3c023
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-19 11:31:58 +0100

    firebase/auth/graphql basic logic added to client package

---
+++ client/src/graphql/index.ts
@@ -0,0 +1,38 @@
+import { ApolloClient, createHttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { currentUser } from "../auth";
+
+let clientInstance:  ApolloClient<NormalizedCacheObject> | null = null;
+
+const initApolloClient = (uri: string) => {
+  const httpLink = createHttpLink({
+    uri,
+  });
+
+  const authLink = setContext(async (_, { headers }) => {
+    const user = await currentUser();
+    let idToken: string = undefined;
+    if (user != null) {
+      idToken = await user.getIdToken();
+    }
+
+    return {
+      headers: {
+        ...headers,
+        idToken,
+      },
+    };
+  });
+
+  clientInstance = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+};
+
+export const index = () => {
+  if (clientInstance == null)
+    throw "Client is not initialized. Run initApolloClient first!";
+
+  return clientInstance;
+};
