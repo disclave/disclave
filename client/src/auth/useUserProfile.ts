0829bccbb348e1431ec6f5b45b5b26704b794285
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-22 13:49:43 +0100

    fetch user profile in use profile hook

--- client/src/auth/useUserProfile.ts
+++ client/src/auth/useUserProfile.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { onAuthStateChanged, UserProfileModel } from "./";
+import { getSelfProfile } from "../modules/users";
 
 type Loading = boolean;
 type UseUserProfile = [UserProfileModel | null, Loading];
@@ -12,18 +13,30 @@ export const useUserProfile = (): UseUserProfile => {
   useEffect(() => onAuthStateChanged(setUser));
 
   useEffect(() => {
+    setLoading(true);
+
+    const updateProfile = (newProfile: UserProfileModel | null) => {
+      setProfile(newProfile);
+      setLoading(false);
+    };
+
     if (user == null) {
-      setProfile(null);
-    } else {
-      setProfile({
-        uid: user.uid,
-        email: user.email,
-        name: user.email, // TODO: fetch user profile from DB and get user name instead of email
-        profileFillPending: false, // TODO: fill based on user profile in DB
-      });
+      updateProfile(null);
+      return;
     }
 
-    setLoading(false);
+    const fetchUserProfile = async (user) => {
+      const result = await getSelfProfile();
+
+      const profile = {
+        uid: user.id,
+        email: user.email,
+        name: result != null ? result.name : "",
+        profileFillPending: result == null,
+      };
+
+      updateProfile(profile);
+    };
   }, [user?.uid]);
 
   return [profile, loading];
