eee5b06e8a2d4fd85c3915d7bc444c91c577645c
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-22 09:21:53 +0100

    authentication in chrome extension

--- chrome-extension/src/pages/Home.tsx
+++ chrome-extension/src/pages/Home.tsx
@@ -2,10 +2,14 @@ import * as React from "react";
 import { CommentsContainer } from "@webchat/ui";
 import { useComments } from "../hooks";
 import { loginHref } from "./Login";
+import { useUserProfile } from "@webchat/client";
 
 export const homeHref = "/";
 
 export const Home = () => {
+  const [userProfile] = useUserProfile();
+  const authenticated = userProfile != null;
+
   const [comments, addComment] = useComments();
 
   if (comments == null) return <div>loading</div>;
@@ -13,7 +17,7 @@ export const Home = () => {
   return (
     <CommentsContainer
       className="max-h-96"
-      authenticated={false}
+      authenticated={authenticated}
       comments={comments}
       loginHref={loginHref}
       onSubmit={addComment}
--- chrome-extension/src/pages/Login.tsx
+++ chrome-extension/src/pages/Login.tsx
@@ -1,19 +1,23 @@
 import * as React from "react";
+import { useHistory } from "react-router-dom";
 import { LoginFormContainer } from "@webchat/ui";
-import { login, logout } from "@webchat/client";
+import { login, logout, useUserProfile } from "@webchat/client";
+import { homeHref } from "./Home";
 
 export const loginHref = "/login";
 
 export const Login = () => {
-  // TODO: fetch from context
-  let userProfile = null;
+  const history = useHistory();
+  const [userProfile, loadingProfile] = useUserProfile();
 
   const onLogin = async (email: string, password: string) => {
-    // await login(email, password);
+    await login(email, password);
+    await history.push(homeHref);
   };
 
   const onLogout = async () => {
-    // await logout();
+    await logout();
+    await history.push(homeHref);
   };
 
   return (
@@ -21,7 +25,7 @@ export const Login = () => {
       <LoginFormContainer
         onLogin={onLogin}
         onLogout={onLogout}
-        userProfile={userProfile}
+        userProfile={!loadingProfile ? userProfile : undefined}
       />
     </div>
   );
