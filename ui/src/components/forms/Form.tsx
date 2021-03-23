9c9af9776701bdc4bd60a4cf66a10a925808ed0f
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-23 10:55:48 +0100

    form onSubmit prop added

--- ui/src/components/forms/Form.tsx
+++ ui/src/components/forms/Form.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-export interface FormProps {}
+export interface FormProps {
+  onSubmit?: React.FormEventHandler<HTMLFormElement>;
+}
 
 export const Form: React.FC<FormProps> = (props) => {
-  return <form>{props.children}</form>;
+  return <form onSubmit={props.onSubmit}>{props.children}</form>;
 };
