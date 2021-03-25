de4b0fc499caee7aba3be218ce6b0d3235e684a0
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-25 11:44:07 +0100

    website view refactored

--- website/src/pages/website/[website]/index.tsx
+++ website/src/pages/website/[website]/index.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/router';
 import { GetServerSideProps } from 'next';
 import { CommentsContainer } from '@webchat/ui';
-import { loginHref } from '../auth/login';
-import { createComment, CommentModel, useSession } from '@webchat/client';
-import { getCommentService, init } from '@webchat/server';
-import { registerHref } from '../auth/register';
+import { loginHref } from '../../auth/login';
+import { CommentModel, useSession } from '@webchat/client';
+import { registerHref } from '../../auth/register';
+import { getComments, useComments } from '../../../modules/comments';
+import { initServer } from '../../../modules/server';
 
 export const websiteCommentsHref = (url: string) => websiteCommentsHrefRaw + url;
 export const websiteCommentsHrefRaw = '/comments/';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  init(JSON.parse(process.env.FIREBASE_CERT));
-  const service = getCommentService();
+  initServer();
   const { website } = context.query;
-  const comments = await service.getComments(website as string);
+  const comments = await getComments(website as string);
 
   return {
     props: {
@@ -27,22 +27,13 @@ interface WebsiteProps {
   comments: Array<CommentModel>;
 }
 
-const Website: React.FC<WebsiteProps> = (props) => {
+const Index: React.FC<WebsiteProps> = (props) => {
   const [, , isActiveAccount] = useSession();
 
   const router = useRouter();
   const website = router.query.website as string;
 
-  const [comments, setComments] = useState(props.comments);
-
-  const onCommentAdd = async (text: string) => {
-    try {
-      const addedComment = await createComment(text, website);
-      setComments([addedComment, ...comments]);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  const [comments, addComment] = useComments(props.comments, website);
 
   const headerHeight = '48px';
 
@@ -62,11 +53,11 @@ const Website: React.FC<WebsiteProps> = (props) => {
             className="max-h-full"
             loginHref={loginHrefWithRedirect}
             registerHref={registerHrefWithRedirect}
-            onSubmit={onCommentAdd}
+            onSubmit={addComment}
           />
         </div>
       </main>
     </div>
   );
 };
-export default Website;
+export default Index;
