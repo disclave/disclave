248094d5d92232d5a01cf84453fd59c3ccd7fd1f
JKrol <github@kroljakub.pl>
2021-03-26 15:03:58 +0100

    logo added to header

--- website/src/modules/layout/header/Header.tsx
+++ website/src/modules/layout/header/Header.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const Header: React.VFC = () => {
   return (
     <header className="sticky top-0 bg-white lg:px-12 h-20 flex flex-row items-center">
-      <div>Logo</div>
+      <Link href="/">
+        <a>
+          <Image
+            src="/logo/base_logo_transparent.png"
+            alt=""
+            width={134}
+            height={80}
+            priority={true}
+            quality={100}
+          />
+        </a>
+      </Link>
       <nav className="flex-grow text-right">navigation</nav>
       <div>user</div>
     </header>
