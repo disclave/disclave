8de9a45d35934ac4567be09ceaa293c0af5afc3b
Jakub Król <jakub.krol@mc2innovations.com>
2021-03-26 09:16:11 +0100

    target update

--- website/next.config.js
+++ website/next.config.js
@@ -2,7 +2,7 @@ const { i18n } = require('./next-i18next.config');
 
 module.exports = {
   i18n,
-  target: 'experimental-serverless-trace',
+  target: 'server',
   env: {
     FIREBASE_CLIENT_CONFIG: process.env.FIREBASE_CLIENT_CONFIG
   }
