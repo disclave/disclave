5a7f8837d6e365967c1e9092303cb19731363f7e
Jakub Król <github@kroljakub.pl>
2021-03-28 18:57:33 +0200

    #62 transaction helpers added

--- server/src/mongodb/index.ts
+++ server/src/mongodb/index.ts
@@ -1,4 +1,4 @@
-import { Db, MongoClient } from "mongodb";
+import { Db, MongoClient, ClientSession, WithTransactionCallback } from "mongodb";
 
 let _client: MongoClient | null = null;
 let _db: Db | null = null;
@@ -21,5 +21,28 @@ export const db = (): Db => {
   return _db;
 }
 
-export type {Db};
+export const withSession = async <T>(run: (session: ClientSession) => T | Promise<T>): Promise<T> => {
+  if (!_client)
+    throw "Database connection is not initialized!";
+
+  // TODO: waiting for https://jira.mongodb.org/browse/NODE-2014
+  let result: T;
+  await _client.withSession(async (session: ClientSession) => {
+    result = await run(session);
+  });
+  return result;
+}
+
+export const withTransaction = async <T>(run: WithTransactionCallback<T>): Promise<T> => {
+  return await withSession(async (session: ClientSession): Promise<T> => {
+    // TODO: waiting for https://jira.mongodb.org/browse/NODE-2014
+    let result: T;
+    await session.withTransaction(async (session: ClientSession) => {
+      result = await run(session);
+    })
+    return result;
+  });
+}
+
+export type {Db, ClientSession, WithTransactionCallback};
 
