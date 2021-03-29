bbcf482e59528b6fae0bb4ec2d343bcb08513d26
JKrol <github@kroljakub.pl>
2021-03-29 11:00:11 +0200

    export ui button

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -3,5 +3,7 @@ import "./index.css";
 export { changeLanguage } from "./i18n";
 export { setAnchorWrapper } from "./config";
 
+export { Button } from "./components/button";
+
 export { LoginFormContainer, RegisterFormContainer } from "./components/auth";
 export { CommentsContainer } from "./components/comments";
