bd2eb3d34824fdf575698657cbb2e9b3dcb44780
JKrol <github@kroljakub.pl>
2021-03-31 15:47:30 +0200

    TODOs added

--- website/src/pages/pwa-share.tsx
+++ website/src/pages/pwa-share.tsx
@@ -6,11 +6,13 @@ const PwaShare = () => {
   const router = useRouter();
   const link = router.query.link;
 
+  // TODO: handle errors
   if (link && typeof link === 'string') {
     const href = websiteHref(link);
     router.push(href);
   }
 
+  // TODO: use Layout and translations
   return <div>Loading...</div>;
 };
 export default PwaShare;
