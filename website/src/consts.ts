12494449914fc0c3e791067f6bacc9528150db12
JKrol <github@kroljakub.pl>
2021-04-01 09:14:44 +0200

    seo data added to consts

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -3,3 +3,40 @@ export const disclaveMessenger = 'https://m.me/disclave';
 export const disclaveGithub = 'https://github.com/disclave';
 export const disclaveTwitter = 'https://twitter.com/disclave_';
 export const disclaveReddit = 'https://reddit.com/u/disclave';
+
+// TODO: modify to use translations
+// TODO: verify openGraph and twitter
+export const seo = {
+  title: 'Disclave',
+  description: 'Use Disclave to comment on any website on the internet!',
+  canonical: 'https://disclave.com/',
+  openGraph: {
+    url: 'https://disclave.com/',
+    title: 'Disclave',
+    description: 'Use Disclave to comment on any website on the internet!',
+    images: [
+      {
+        url: 'https://disclave.com/logo/base_logo_white_3125x1875.png',
+        width: 3125,
+        height: 1875,
+        alt: 'Disclave'
+      },
+      {
+        url: 'https://disclave.com/logo/base_logo_white_1200x720.png',
+        width: 1200,
+        height: 720,
+        alt: 'Disclave'
+      },
+      {
+        url: 'https://disclave.com/logo/base_logo_white_800x480.png',
+        width: 800,
+        height: 480,
+        alt: 'Disclave'
+      }
+    ]
+  },
+  twitter: {
+    handle: 'disclave_',
+    cardType: 'summary_large_image'
+  }
+};
