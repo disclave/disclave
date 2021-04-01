b605674faa6dd2594903bbf291735d16def33f68
JKrol <github@kroljakub.pl>
2021-04-01 09:24:17 +0200

    duplicated tag removed

--- website/src/modules/head/AppHead.tsx
+++ website/src/modules/head/AppHead.tsx
@@ -8,8 +8,6 @@ export const AppHead: React.VFC = () => {
 
   return (
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-
       <link rel="icon" href="/favicon.ico" />
       <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
       <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
