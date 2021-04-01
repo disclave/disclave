f8803659a10e05b0b30dfab15b519212157a3588
JKrol <github@kroljakub.pl>
2021-04-01 10:39:05 +0200

    offline support added

--- website/public/sw.js
+++ website/public/sw.js
@@ -1,19 +1,48 @@
-const CACHE_NAME = 'disclave-pwa-cache-v1';
-// TODO: configure cache
-const urlsToCache = [];
+const OFFLINE_VERSION = 1;
+const OFFLINE_CACHE = 'disclave-offline-cache-v1';
+const OFFLINE_URL = 'offline.html';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    (async () => {
+      const cache = await caches.open(OFFLINE_CACHE);
+      await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
+    })()
   );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      if ('navigationPreload' in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          return await fetch(event.request);
+        } catch (error) {
+          console.log('Fetch failed; returning offline page instead.', error);
+
+          const cache = await caches.open(OFFLINE_CACHE);
+          return await cache.match(OFFLINE_URL);
+        }
+      })()
+    );
+  }
 });
