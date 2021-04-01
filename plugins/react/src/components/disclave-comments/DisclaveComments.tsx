6f7afc4ec58381d67f69b0838a20918f7919e63c
JKrol <github@kroljakub.pl>
2021-04-01 15:47:36 +0200

    fetch comments count on new comment

--- plugins/react/src/components/disclave-comments/DisclaveComments.tsx
+++ plugins/react/src/components/disclave-comments/DisclaveComments.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getIframeUrl } from "../../helpers/UrlHelper";
 import styles from "./DisclaveComments.module.css";
 import { countComments } from "../../api";
+import { useMessageListener } from "../../helpers/useMessageListener";
 
 export interface DisclaveCommentsProps {
   height?: string;
@@ -10,38 +11,42 @@ export interface DisclaveCommentsProps {
 export const DisclaveComments: React.VFC<DisclaveCommentsProps> = ({
   height = "350",
 }) => {
-  const [href, setHref] = useState<string>();
+  const [url, setUrl] = useState<string>();
   const [count, setCount] = useState<number>(0);
   const [iframeHeight, setIframeHeight] = useState<string>(height);
 
-  useEffect(() => {
-    setHref(window.location.href);
+  const getCommentsCount = async () => {
+    if (!url) return;
+
+    const result = await countComments(url);
+    setCount(result);
+  };
+
+  const onNewMessage = () => getCommentsCount();
+
+  useMessageListener((ev) => {
+    if (ev.data == "disclave-new-message") onNewMessage();
   });
 
   useEffect(() => {
-    if (!href) return;
+    setUrl(window.location.href);
+  });
 
-    const getCommentsCount = async () => {
-      const result = await countComments(href);
-      setCount(result);
-    };
-
+  useEffect(() => {
     getCommentsCount();
-  }, [href]);
+  }, [url]);
 
   useEffect(() => {
     if (count == 0) setIframeHeight("200");
+    else if (count > 7) setIframeHeight("750");
     else setIframeHeight(height);
   }, [count]);
 
-  if (!href) return <div>Loading...</div>;
+  if (!url) return <div>Loading...</div>;
 
   return (
     <div className={styles.wrapper} style={{ height: iframeHeight + "px" }}>
-      <iframe
-        src={getIframeUrl(href, iframeHeight)}
-        className={styles.iframe}
-      />
+      <iframe src={getIframeUrl(url, iframeHeight)} className={styles.iframe} />
     </div>
   );
 };
