e82fbf3b563db5521f5f86180f1706546d451e85
JKrol <github@kroljakub.pl>
2021-04-01 15:11:42 +0200

    scale react plugin with comments count

--- plugins/react/src/api/index.ts
+++ plugins/react/src/api/index.ts
@@ -1,13 +1,16 @@
 const apiUrl = process.env.API_URL as string;
 
 export const countComments = async (url: string): Promise<number> => {
+  const queryName = "countComments";
+
   const request = {
-    query: "countComments",
+    query: `query ($url: String!) { ${queryName}(url: $url) }`,
+    variables: { url: url },
   };
   const response = await fetch(apiUrl, {
     method: "POST",
     body: JSON.stringify(request),
   });
-  const count = response.text();
-  return Number(count);
+  const result = await response.json();
+  return Number(result.data[queryName]);
 };
--- plugins/react/src/components/disclave-comments/DisclaveComments.tsx
+++ plugins/react/src/components/disclave-comments/DisclaveComments.tsx
@@ -1,25 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { getIframeUrl } from "../../helpers/UrlHelper";
 import styles from "./DisclaveComments.module.css";
+import { countComments } from "../../api";
 
 export interface DisclaveCommentsProps {
   height?: string;
 }
 
 export const DisclaveComments: React.VFC<DisclaveCommentsProps> = ({
-  height = "250",
+  height = "350",
 }) => {
   const [href, setHref] = useState<string>();
+  const [count, setCount] = useState<number>(0);
+  const [iframeHeight, setIframeHeight] = useState<string>(height);
 
   useEffect(() => {
     setHref(window.location.href);
   });
 
+  useEffect(() => {
+    if (!href) return;
+
+    const getCommentsCount = async () => {
+      const result = await countComments(href);
+      setCount(result);
+    };
+
+    getCommentsCount();
+  }, [href]);
+
+  useEffect(() => {
+    if (count == 0) setIframeHeight("200");
+    else setIframeHeight(height);
+  }, [count]);
+
   if (!href) return <div>Loading...</div>;
 
   return (
-    <div className={styles.wrapper} style={{ height: height + "px" }}>
-      <iframe src={getIframeUrl(href, height)} className={styles.iframe} />
+    <div className={styles.wrapper} style={{ height: iframeHeight + "px" }}>
+      <iframe
+        src={getIframeUrl(href, iframeHeight)}
+        className={styles.iframe}
+      />
     </div>
   );
 };
