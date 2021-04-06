45d43ff2f902937889c6fc3dec58936663e30512
JKrol <github@kroljakub.pl>
2021-04-06 15:00:01 +0200

    DOMAIN update

--- website/next.config.js
+++ website/next.config.js
@@ -1,10 +1,17 @@
 const { i18n } = require('./next-i18next.config');
 
+const getDomain = () => {
+  let domain = process.env.DOMAIN;
+  if (!domain) return domain;
+
+  return domain.replace('${HEROKU_APP_NAME}', process.env.HEROKU_APP_NAME);
+};
+
 module.exports = {
   i18n,
   target: 'server',
   env: {
-    DOMAIN: process.env.DOMAIN,
+    DOMAIN: getDomain(),
     FIREBASE_CLIENT_CONFIG: process.env.FIREBASE_CLIENT_CONFIG
   },
   future: {
