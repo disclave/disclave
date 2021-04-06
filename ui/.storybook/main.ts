abe8e8e477c0e88ceab4aa62c5f78ec9fc9b8a39
JKrol <github@kroljakub.pl>
2021-04-06 14:52:17 +0200

    postcss8 config

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -3,7 +3,18 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\,css&/,
