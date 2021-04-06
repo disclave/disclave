e52d42d1ed889fcf07be2104561243d459e948e3
JKrol <github@kroljakub.pl>
2021-04-06 14:21:49 +0200

    build fix

--- chrome-extension/src/init.ts
+++ chrome-extension/src/init.ts
@@ -1,4 +1,4 @@
 import { init } from "@disclave/client";
 
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CLIENT_CONFIG as string);
-init(firebaseConfig, process.env.API_URL as string);
+init(firebaseConfig, process.env.API_URL as string, false);
