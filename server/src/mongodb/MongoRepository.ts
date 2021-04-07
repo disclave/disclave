7f9ab849b97b339f340fab33c2750e9523642295
JKrol <github@kroljakub.pl>
2021-04-07 12:05:53 +0200

    di bug fix

--- server/src/mongodb/MongoRepository.ts
+++ server/src/mongodb/MongoRepository.ts
@@ -1,5 +1,7 @@
 import { ClientSession, withTransaction } from "./index";
+import { injectable } from "inversify";
 
+@injectable()
 export abstract class MongoRepository {
   public async runTransaction(
     run: (session: ClientSession) => Promise<unknown>
