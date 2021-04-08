24afe687536d3693905c0c46fe8fe167476ad916
JKrol <github@kroljakub.pl>
2021-04-08 14:26:18 +0200

    email verification screnn added

--- ui/src/components/auth/register/form/email-verification/EmailVerificationForm.tsx
+++ ui/src/components/auth/register/form/email-verification/EmailVerificationForm.tsx
@@ -1,34 +1,46 @@
 import React from "react";
 import { useLoading } from "@/hooks";
 import { useTranslation } from "@/i18n";
+import { Button } from "@/components/button";
 
 export interface EmailVerificationFormProps {
+  userEmail: string;
   onSendEmailVerification: () => Promise<void>;
   onLogout: () => Promise<void>;
 }
 
-// TODO: add stories
 export const EmailVerificationForm: React.VFC<EmailVerificationFormProps> = (
   props
 ) => {
   const { t } = useTranslation("auth");
   const [loading, runWithLoading, error] = useLoading(false);
 
-  const onSubmit = async () => {
-    // // TODO: verify for valid name characters
-    // await runWithLoading(() => props.onSubmit(data.name));
+  const onSendEmail = async () => {
+    await runWithLoading(() => props.onSendEmailVerification());
   };
 
   const onLogoutClick = async () => {
     await runWithLoading(() => props.onLogout());
   };
 
-  // TODO: finish HTML
   return (
     <div>
-      <p>Display info to check email</p>
-      <p>Allow to send again (button)</p>
-      <p>Allow to logout (button)</p>
+      <p className="p-4 text-white text-lg bg-green-500 rounded">
+        {t("register.email verification.information", {
+          email: props.userEmail,
+        })}
+      </p>
+      <p className="pt-4 pb-2">
+        {t("register.email verification.missing email")}
+      </p>
+      <div className="flex flex-row space-x-2">
+        <Button onClick={onSendEmail} outlined disabled={loading}>
+          {t("register.email verification.button.send email")}
+        </Button>
+        <Button onClick={onLogoutClick} flat disabled={loading}>
+          {t("register.email verification.button.use different account")}
+        </Button>
+      </div>
     </div>
   );
 };
