dc41a61ee3015578ec104cd67186ba39361d7696
JKrol <github@kroljakub.pl>
2021-04-08 11:11:00 +0200

    client schema fix

--- client/src/modules/comments/schemas.ts
+++ client/src/modules/comments/schemas.ts
@@ -32,13 +32,19 @@ export const CREATE_COMMENT = gql`
 `;
 
 export const REMOVE_COMMENT_VOTE = gql`
-  mutation(commentId: ID) { removeCommentVote(commentId: $commentId) }
+  mutation($commentId: ID) {
+    removeCommentVote(commentId: $commentId)
+  }
 `;
 
 export const ADD_COMMENT_VOTE_UP = gql`
-  mutation(commentId: ID) { addCommentVoteUp(commentId: $commentId) }
+  mutation($commentId: ID) {
+    addCommentVoteUp(commentId: $commentId)
+  }
 `;
 
 export const ADD_COMMENT_VOTE_DOWN = gql`
-  mutation(commentId: ID) { addCommentVoteDown(commentId: $commentId) }
+  mutation($commentId: ID) {
+    addCommentVoteDown(commentId: $commentId)
+  }
 `;
