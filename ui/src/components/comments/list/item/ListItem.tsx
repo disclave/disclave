c164549a77d53e9644fad200fa8f38bcf78bc9cf
JKrol <github@kroljakub.pl>
2021-04-13 14:10:12 +0200

    Comment Item - show more button added and link on date

--- ui/src/components/comments/list/item/ListItem.tsx
+++ ui/src/components/comments/list/item/ListItem.tsx
@@ -4,11 +4,13 @@ import { DateTimePreview } from "@/components/date/dateTimePreview";
 import { CommentVote } from "@/components/comments/list/item/vote";
 import { CommentWebsiteInfo } from "@/components/comments/list/item/website";
 import classNames from "classnames";
+import { getAnchorWrapper } from "@/config";
+import { useTranslation } from "@/i18n";
 
 export interface ListItemProps {
   comment: CommentModel;
   authenticated: boolean;
-  hrefBuilder?: (urlMeta: CommentUrlMeta) => string;
+  hrefBuilder?: (urlMeta: CommentUrlMeta, commentId?: string) => string;
   preview: boolean;
   showWebsite: boolean;
   onVoteUp: (commentId: string) => Promise<void>;
@@ -17,9 +19,16 @@ export interface ListItemProps {
 }
 
 export const ListItem: React.VFC<ListItemProps> = (props) => {
+  const { t } = useTranslation("comments");
   const [showMoreVisible, setShowMoreVisible] = useState(false);
   const textWrapperRef = useRef<HTMLDivElement>(null);
   const textRef = useRef<HTMLParagraphElement>(null);
+
+  const detailsHref = props.hrefBuilder
+    ? props.hrefBuilder(props.comment.urlMeta, props.comment.id)
+    : undefined;
+
+  const AnchorTag = getAnchorWrapper() ?? "a";
 
   const textWrapperClassNames = classNames({
     "max-h-10 overflow-hidden": props.preview,
@@ -32,6 +41,26 @@ export const ListItem: React.VFC<ListItemProps> = (props) => {
       textRef.current.scrollHeight > textWrapperRef.current.offsetHeight
     );
   });
+
+  const DateTime = () => (
+    <span className="ml-2 font-light text-xs">
+      <DateTimePreview iso={props.comment.timestamp} />
+    </span>
+  );
+  const DateTimeLink = ({ href }: { href: string }) => (
+    <AnchorTag className="hover:underline" href={href}>
+      <DateTime />
+    </AnchorTag>
+  );
+
+  const ShowMoreLink = ({ href }: { href: string }) => (
+    <AnchorTag
+      href={href}
+      className="uppercase text-primary text-xs hover:underline"
+    >
+      {t("list.item.show all")}
+    </AnchorTag>
+  );
 
   return (
     <div>
@@ -47,9 +76,7 @@ export const ListItem: React.VFC<ListItemProps> = (props) => {
         <span className="font-semibold text-sm">
           {props.comment.author.name}
         </span>
-        <span className="ml-2 font-light text-xs">
-          <DateTimePreview iso={props.comment.timestamp} />
-        </span>
+        {detailsHref ? <DateTimeLink href={detailsHref} /> : <DateTime />}
       </div>
 
       <div className="mt-1 mb-2">
@@ -61,8 +88,9 @@ export const ListItem: React.VFC<ListItemProps> = (props) => {
           />
         </div>
 
-        {/* TODO: add "show more" button */}
-        {showMoreVisible ? "..." : null}
+        {showMoreVisible && detailsHref ? (
+          <ShowMoreLink href={detailsHref} />
+        ) : null}
       </div>
 
       <div>
