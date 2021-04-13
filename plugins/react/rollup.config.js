1c98b636548b38e30de21e4013af4f14ea459eb5
JKrol <github@kroljakub.pl>
2021-04-13 19:31:06 +0200

    Plugins page fix (#133)
    * react plugin better errors handling
    
    * testing fix with rollup replace
    
    * testing rollup config
    
    * env vars config update
    
    * readme update

--- plugins/react/rollup.config.js
+++ plugins/react/rollup.config.js
@@ -1,11 +1,15 @@
 import babel from "@rollup/plugin-babel";
 import del from "rollup-plugin-delete";
-import dotEnv from "rollup-plugin-dotenv";
 import external from "rollup-plugin-peer-deps-external";
 import json from "@rollup/plugin-json";
 import pkg from "./package.json";
 import postcss from "rollup-plugin-postcss-modules";
 import typescript from "@rollup/plugin-typescript";
+import { config } from 'dotenv';
+import replace from '@rollup/plugin-replace';
+
+const parsed = config().parsed || {};
+parsed.IFRAME_URL = parsed.IFRAME_URL || process.env.IFRAME_URL;
 
 export default {
   input: "src/index.ts",
@@ -15,7 +19,11 @@ export default {
   ],
   plugins: [
     del({ targets: ["dist/*"] }),
-    dotEnv(),
+    replace({
+      process: JSON.stringify({
+        env: parsed
+     }),
+    }),
     postcss({
       extract: false,
       modules: true,
--- plugins/react/src/components/disclave-comments/DisclaveComments.tsx
+++ plugins/react/src/components/disclave-comments/DisclaveComments.tsx
@@ -4,18 +4,26 @@ import styles from "./DisclaveComments.module.css";
 import { useIframeHeightListener } from "../../iframe";
 
 export const DisclaveComments: React.VFC = () => {
-  const [url, setUrl] = useState<string>();
+  const [url, setUrl] = useState<string | null>(null);
   const iframeHeight = useIframeHeightListener("200");
 
   useEffect(() => {
-    setUrl(window.location.href);
+    const locationHref = window?.location?.href;
+    if (!locationHref) return;
+
+    try {
+      const iframeUrl = getIframeUrl(locationHref);
+      setUrl(iframeUrl);
+    } catch (e) {
+      console.error(e);
+    }
   });
 
   if (!url) return <div>Loading...</div>;
 
   return (
     <div className={styles.wrapper} style={{ height: iframeHeight + "px" }}>
-      <iframe src={getIframeUrl(url)} className={styles.iframe} />
+      <iframe src={url} className={styles.iframe} />
     </div>
   );
 };
