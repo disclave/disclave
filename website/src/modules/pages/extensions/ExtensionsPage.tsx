7835cc17c5d48e56544b1020d85ca25b8110df10
JKrol <github@kroljakub.pl>
2021-04-14 13:04:18 +0200

    Preparing Exteionsion page

---
+++ website/src/modules/pages/extensions/ExtensionsPage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Layout } from '@/modules/layout';
+import { PluginTile } from '@/modules/plugins';
+import { reactPluginHref } from '@/consts';
+import { useTranslation } from 'next-i18next';
+
+export const ExtensionsPage: React.VFC = () => {
+  const { t } = useTranslation('plugins');
+
+  return (
+    <Layout>
+      <section className="container mx-auto py-8">
+        <div className="mx-4">
+          <div>
+            <h1 className="text-3xl pb-4">{t('main.header')}</h1>
+            <p className="whitespace-pre-line">{t('main.text')}</p>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center my-6">
+            <PluginTile
+              href={reactPluginHref}
+              name={t('plugins list.react.name')}
+              logoSrc="/images/plugins/react-logo.png"
+            />
+            <PluginTile name={t('plugins list.more soon')} />
+          </div>
+          <div>
+            <p>{t('plugins list.not found.text')}</p>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+};
---
+++ website/src/modules/pages/extensions/index.ts
@@ -0,0 +1,1 @@
+export { ExtensionsPage } from './ExtensionsPage';
