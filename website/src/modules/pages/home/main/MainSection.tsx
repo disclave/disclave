51220a98012223a16d66174f70c287c0ad90f019
JKrol <github@kroljakub.pl>
2021-04-14 14:13:35 +0200

    refactor

--- website/src/modules/pages/home/main/MainSection.tsx
+++ website/src/modules/pages/home/main/MainSection.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 import { Button, FormErrorContainer, FormFactory, TextField } from '@disclave/ui';
 import { useLoading } from '@disclave/ui';
