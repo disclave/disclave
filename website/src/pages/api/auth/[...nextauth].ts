83207b90626c5ae2ce9a128ea8c7777408786280
JKrol <github@kroljakub.pl>
2021-04-26 10:43:35 +0200

    generating verification code

--- website/src/pages/api/auth/[...nextauth].ts
+++ website/src/pages/api/auth/[...nextauth].ts
@@ -11,8 +11,17 @@ export default NextAuth({
       sendVerificationRequest: async ({ identifier: email, url, token, baseUrl, provider }) => {
         const emailService = getEmailService();
         await emailService.sendAuthVerificationCodeEmail(email, token);
+      },
+      generateVerificationToken: async (): Promise<string> => {
+        const length = 6;
+        const characters = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789'; // A-Z1-9 (O and 0 removed)
+        const charactersLength = characters.length;
+        const result = [];
+        for (let i = 0; i < length; i++)
+          result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
+        return result.join('');
       }
-    })
+    } as any) // TODO: remove cast to any when next-auth types fixed
   ],
   database: process.env.DB_URI
 });
