f8d1cdedee8a2213bf377d3df407e30187f41a85
JKrol <github@kroljakub.pl>
2021-04-27 10:13:33 +0200

    config fix

--- website/next.config.js
+++ website/next.config.js
@@ -1,10 +1,10 @@
 const { i18n } = require('./next-i18next.config');
 
-const getDomain = () => {
-  let domain = process.env.DOMAIN;
-  if (!domain) return domain;
-
-  return domain.replace('${HEROKU_APP_NAME}', process.env.HEROKU_APP_NAME);
+const getDomain = () => replaceHerokuAppName(process.env.DOMAIN);
+const getNextAuthUrl = () => replaceHerokuAppName(process.env.NEXTAUTH_URL);
+const replaceHerokuAppName = (url) => {
+  if (!url) return url;
+  return url.replace('${HEROKU_APP_NAME}', process.env.HEROKU_APP_NAME);
 };
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
   target: 'server',
   env: {
     DOMAIN: getDomain(),
-    NEXTAUTH_URL: getDomain(),
+    NEXTAUTH_URL: getNextAuthUrl(),
     FIREBASE_CLIENT_CONFIG: process.env.FIREBASE_CLIENT_CONFIG
   },
   future: {
