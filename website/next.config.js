1225846b556a9db99939a1e31d33c547aea49b87
JKrol <github@kroljakub.pl>
2021-04-27 10:32:58 +0200

    testing modified env config

--- website/next.config.js
+++ website/next.config.js
@@ -1,18 +1,11 @@
 const { i18n } = require('./next-i18next.config');
 
-const getDomain = () => replaceHerokuAppName(process.env.DOMAIN);
-const getNextAuthUrl = () => replaceHerokuAppName(process.env.NEXTAUTH_URL);
-const replaceHerokuAppName = (url) => {
-  if (!url) return url;
-  return url.replace('${HEROKU_APP_NAME}', process.env.HEROKU_APP_NAME);
-};
-
 module.exports = {
   i18n,
   target: 'server',
   env: {
-    DOMAIN: getDomain(),
-    NEXTAUTH_URL: getNextAuthUrl(),
+    DOMAIN: process.env.DOMAIN,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     FIREBASE_CLIENT_CONFIG: process.env.FIREBASE_CLIENT_CONFIG
   },
   future: {
