2f11a2b0a2a7583aa0d8dbd3cb21dd13e9d4de10
JKrol <github@kroljakub.pl>
2021-05-10 15:25:29 +0200

    rollback

--- website/src/pages/auth/login.tsx
+++ website/src/pages/auth/login.tsx
@@ -1,7 +1,6 @@
 import { valuesToParamsArray } from '@/modules/redirect';
 import { LoginPage } from '@/modules/pages/auth/login';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React from 'react';
 
 export const loginHref = (redirectPath?: string, redirectPathParamToEncode?: string): string => {
   let path = '/auth/login';
