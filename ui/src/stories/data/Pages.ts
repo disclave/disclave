5f2bb643334d8cd283755e1c7b1eafb33e923cdd
JKrol <github@kroljakub.pl>
2021-05-12 14:00:59 +0200

    pages list component added

--- ui/src/stories/data/Pages.ts
+++ ui/src/stories/data/Pages.ts
@@ -1,4 +1,30 @@
 import { PageModel } from "@/components/pages/PageModel";
+import { randomInt } from "./helpers";
+
+export interface BuildExamplePageProps {
+  websiteId?: string;
+  pageId?: string;
+  commentsCount?: number;
+}
+
+export const buildExamplePage = ({
+  websiteId = "example.com",
+  pageId = "%2Fpage%2Fwith%2Fexample%2Furl",
+  commentsCount = randomInt(1, 10000),
+}: BuildExamplePageProps): PageModel => ({
+  id: websiteId + pageId + Math.random(),
+  websiteId: websiteId,
+  pageId: pageId,
+  commentsCount: commentsCount,
+});
+
+export const RandomPagesList = (size: number): PageModel[] => {
+  const result: PageModel[] = [];
+  while (size--) {
+    result.push(buildExamplePage({}));
+  }
+  return result;
+};
 
 export const ExamplePage: PageModel = {
   id: "example.com%2Fpath",
---
+++ ui/src/stories/data/helpers.ts
@@ -0,0 +1,11 @@
+export const randomDate = (start: Date, end: Date): Date => {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+};
+
+export const randomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
