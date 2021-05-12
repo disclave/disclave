890fdf34878ab96c8036aee6c647292a8a1c6479
JKrol <github@kroljakub.pl>
2021-05-12 15:52:23 +0200

    display fix

--- ui/src/components/links/LinkBox.tsx
+++ ui/src/components/links/LinkBox.tsx
@@ -9,7 +9,7 @@ export const LinkBox: React.FC<LinkBoxProps> = ({ children, href }) => {
   const AnchorTag = getAnchorWrapper() ?? "a";
 
   return (
-    <AnchorTag href={href}>
+    <AnchorTag href={href} className="block">
       <div className="rounded hover:bg-gray-100">{children}</div>
     </AnchorTag>
   );
