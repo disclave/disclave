92f0a202a38894cd827ff9ee207efd723df8146c
JKrol <github@kroljakub.pl>
2021-05-12 09:17:02 +0200

    tranlsations added

--- website/src/modules/pages/home/extensions/ExtensionsSection.tsx
+++ website/src/modules/pages/home/extensions/ExtensionsSection.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Button } from '@disclave/ui';
 import { extensionsHref } from '@/pages/extensions';
+import { useTranslation } from 'next-i18next';
 
 export interface ExtensionsSectionProps {
   className?: string;
 }
 
 export const ExtensionsSection: React.VFC<ExtensionsSectionProps> = (props) => {
+  const { t } = useTranslation(['home']);
+
   return (
     <section className={props.className}>
-      <h2 className="text-3xl font-semibold">Comment without leaving website</h2>
-      <p className="py-8">
-        Comment any website on the Internet, directly from your browser, without leaving it. Easily read others opinions and let them know, what do you think.
-      </p>
-      <Button href={extensionsHref()} outlined>
-        Install extension
-      </Button>
+      <h2 className="text-3xl font-semibold">{t('extensions.title')}</h2>
+      <p className="py-8">{t('extensions.text')}</p>
+      <Button href={extensionsHref()} outlined>{t('extensions.btn')}</Button>
       <div className="pt-8">
         <img src="/images/home/extensions/1.svg" alt="" width={1061} height={531} />
       </div>
--- website/src/modules/pages/home/plugins/PluginsSection.tsx
+++ website/src/modules/pages/home/plugins/PluginsSection.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Button } from '@disclave/ui';
 import { pluginsHref } from '@/pages/plugins';
+import { useTranslation } from 'next-i18next';
 
 export interface PluginsSectionProps {
   className?: string;
 }
 
 export const PluginsSection: React.VFC<PluginsSectionProps> = (props) => {
+  const { t } = useTranslation(['home']);
+  
   return (
     <section className={props.className}>
-      <h2 className="text-3xl font-semibold">Comments on your website</h2>
-      <p className="py-8">
-        Want to display comments and allow your users to interaccted with them, without leaving
-        your website? Install Disclave plugin and let users promote your website.
-      </p>
-      <Button href={pluginsHref()} outlined>
-        Use plugins
-      </Button>
+      <h2 className="text-3xl font-semibold">{t('plugins.title')}</h2>
+      <p className="py-8">{t('plugins.text')}</p>
+      <Button href={pluginsHref()} outlined>{t('plugins.btn')}</Button>
       <div className="pt-8">
         <img src="/images/home/plugins/1.svg" alt="" width={765} height={484} />
       </div>
