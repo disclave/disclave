f57d517a5874b3ea04c39173a473e19fabafbe19
JKrol <github@kroljakub.pl>
2021-05-18 12:13:33 +0200

    policies configured fot the chrome extension

--- chrome-extension/src/init.ts
+++ chrome-extension/src/init.ts
@@ -1,6 +1,4 @@
 import { init } from "@disclave/client";
+import { apiUrl, domain, firebaseConfig } from "./config";
 
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CLIENT_CONFIG as string);
-const apiUrl = process.env.API_URL as string;
-const domain = process.env.DOOMAIN as string;
 init(firebaseConfig, apiUrl, domain);
