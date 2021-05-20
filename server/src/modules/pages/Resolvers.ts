3e16f24c2053003651a5989ae3d472810774bc3d
JKrol <github@kroljakub.pl>
2021-05-20 13:15:06 +0200

    fetching page details added to gql schema

--- server/src/modules/pages/Resolvers.ts
+++ server/src/modules/pages/Resolvers.ts
@@ -1,4 +1,4 @@
-import { Page } from "./models";
+import { Page, PageDetails } from "./models";
 import { container } from "@/inversify.config";
 import { PageService } from "./index";
 
@@ -13,6 +13,13 @@ export const pagesResolvers = {
       );
       return pages.map(pageToResponse);
     },
+    pageDetails: async (_, args) => {
+      const pageDetails = await service.getPageDetails(
+        args.url,
+        args.fetchIfNoCache
+      );
+      return pageDetailsToResponse(pageDetails);
+    },
   },
 };
 
@@ -22,5 +29,15 @@ const pageToResponse = (page: Page) => {
     websiteId: page.websiteId,
     pageId: page.pageId,
     commentsCount: page.commentsCount,
+  };
+};
+
+const pageDetailsToResponse = (details: PageDetails) => {
+  return {
+    url: details.url,
+    pageId: details.pageId,
+    websiteId: details.websiteId,
+    icon: details.icon,
+    title: details.title,
   };
 };
--- server/src/modules/pages/Schemas.ts
+++ server/src/modules/pages/Schemas.ts
@@ -2,13 +2,22 @@ import { gql } from "apollo-server-micro";
 
 export const pagesTypeDefs = gql`
   type Page {
-    id: ID
-    websiteId: String
-    pageId: String
-    commentsCount: String
+    id: ID!
+    websiteId: String!
+    pageId: String!
+    commentsCount: String!
+  }
+
+  type PageDetails {
+    url: String!
+    pageId: String!
+    websiteId: String!
+    icon: String
+    title: String
   }
 
   extend type Query {
     topCommentedPages(minCommentsVoteSum: Int!, limit: Int!): [Page]
+    pageDetails(url: String, fetchIfNoCache: Boolean!): PageDetails
   }
 `;
