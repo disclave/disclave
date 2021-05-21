6c98e9f53064272dfac7774f53355682289233f4
JKrol <github@kroljakub.pl>
2021-05-21 09:52:23 +0200

    display website icon and title if available

--- website/src/modules/layout/website/info/WebsiteInfo.tsx
+++ website/src/modules/layout/website/info/WebsiteInfo.tsx
@@ -10,12 +10,25 @@ export const WebsiteInfo: React.VFC<WebsiteInfoProps> = ({ pageDetails }) => {
   const { t } = useTranslation('website');
 
   const url = new URL(pageDetails.url);
+  const logo = pageDetails.meta?.logo ?? null;
+  const title = pageDetails.meta?.title ?? null;
 
   return (
     <section className="container mx-auto">
       <div>
-        <div className="text-2xl font-bold break-words">{url.host}</div>
-        <div className="text-lg font-semibold break-words">{url.pathname}</div>
+        <div className="flex flex-row items-center">
+          {logo ? (
+            <div className="pr-1">
+              <img src={logo} alt={url.host} width={24} height={24} />
+            </div>
+          ) : null}
+          <h1 className="text-lg font-bold break-words">{url.host}</h1>
+        </div>
+        {title ? <h2 className="text-2xl break-words">{title}</h2> : null}
+        <h3 className="text-lg font-semibold break-words">
+          {url.pathname}
+          {url.pathname === '/' ? <span className="text-gray-400"> {t('info.main page')}</span> : null}
+        </h3>
         <div className="mt-2">
           <a
             href={pageDetails.url}
