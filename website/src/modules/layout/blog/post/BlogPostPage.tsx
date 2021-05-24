35d1cc4698adb75e4df5af15aaad05accd768c39
JKrol <github@kroljakub.pl>
2021-05-24 13:44:26 +0200

    fix

--- website/src/modules/layout/blog/post/BlogPostPage.tsx
+++ website/src/modules/layout/blog/post/BlogPostPage.tsx
@@ -12,15 +12,15 @@ export const BlogPostPage: React.VFC<BlogPostPageProps> = ({ post }) => {
     <Layout>
       <section className="container mx-auto my-6 px-3">
         <div className="max-w-5xl mx-auto">
-          <DateTimePreview
-            className="text-sm text-gray-400"
-            hideTime
-            iso={post.date}
+          <DateTimePreview className="text-sm text-gray-400" hideTime iso={post.date} />
+          <h1 className="text-3xl font-semibold mb-6">{post.title}</h1>
+          <img
+            src={post.imageSrc}
+            alt={post.title}
+            className="rounded-t"
             width={1200}
             height={600}
           />
-          <h1 className="text-3xl font-semibold mb-6">{post.title}</h1>
-          <img src={post.imageSrc} alt={post.title} className="rounded-t" />
           <p className="my-6" dangerouslySetInnerHTML={{ __html: post.contentHtml }} />
         </div>
       </section>
