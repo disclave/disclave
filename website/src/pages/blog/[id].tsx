d0462c6a3436513bbe6ce4b7a1800ce1a41eeb78
JKrol <github@kroljakub.pl>
2021-05-24 12:07:41 +0200

    basic seo config for blog posts

--- website/src/pages/blog/[id].tsx
+++ website/src/pages/blog/[id].tsx
@@ -1,6 +1,9 @@
+import { domain } from '@/consts';
 import { getPost, getPostIds, Post } from '@/modules/blog';
 import { BlogPostPage } from '@/modules/layout/blog/post';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { NextSeo } from 'next-seo';
 import React from 'react';
 
 export const blogPostHref = (id: string) => `/blog/${id}`;
@@ -10,7 +13,31 @@ interface BlogPostProps {
 }
 
 const BlogPost: React.VFC<BlogPostProps> = (props) => {
-  return <BlogPostPage post={props.post} />;
+  const { t } = useTranslation('blog');
+
+  const seoTitle = t('seo.title');
+  const seoDescription = props.post.title;
+  const seoImg = domain + props.post.imageSrc;
+
+  return (
+    <>
+      <NextSeo
+        title={seoTitle}
+        description={seoDescription}
+        openGraph={{
+          type: 'blog',
+          title: seoTitle,
+          description: seoDescription,
+          images: [
+            {
+              url: seoImg
+            }
+          ]
+        }}
+      />
+      <BlogPostPage post={props.post} />
+    </>
+  );
 };
 export default BlogPost;
 
@@ -24,7 +51,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params, locale }) => {
   const postPromise = getPost(params.id);
-  const translationsPromise = serverSideTranslations(locale, ['common', 'layout']);
+  const translationsPromise = serverSideTranslations(locale, ['blog', 'common', 'layout']);
 
   return {
     props: {
