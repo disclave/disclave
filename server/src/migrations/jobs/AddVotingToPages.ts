9ee51ca94bef4243f81ef8975c2bab67a2efb17c
JKrol <github@kroljakub.pl>
2021-05-25 10:32:24 +0200

    pages voting migration implementation

--- server/src/migrations/jobs/AddVotingToPages.ts
+++ server/src/migrations/jobs/AddVotingToPages.ts
@@ -1,9 +1,28 @@
+import { pagesDbCollection } from "@/database/pages";
 import { Job } from "../Job";
 
 export const AddVotingToPages: Job = {
   id: "AddVotingToPages",
 
   run: async () => {
-    throw new Error("Method not implemented.");
+    console.info("Migrating pages voting.");
+
+    const collection = await pagesDbCollection();
+    const result = await collection.updateMany(
+      {
+        votesSum: { $exists: false },
+      },
+      {
+        $set: {
+          votesUp: [],
+          votesDown: [],
+          votesSum: 0,
+        },
+      }
+    );
+
+    console.info(
+      `Pages voting migration modified ${result.modifiedCount} documents.`
+    );
   },
 };
