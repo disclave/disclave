37556faf2ffe4ca30b83f92e2dc737e076c27f50
JKrol <github@kroljakub.pl>
2021-05-26 11:15:46 +0200

    hideVotes param added to disclave comments

--- plugins/react/src/components/disclave-comments/DisclaveComments.tsx
+++ plugins/react/src/components/disclave-comments/DisclaveComments.tsx
@@ -3,16 +3,24 @@ import { getIframeUrl } from "../../helpers/UrlHelper";
 import styles from "./DisclaveComments.module.css";
 import { useIframeHeightListener } from "../../iframe";
 
-export const DisclaveComments: React.VFC = () => {
+export interface DisclaveCommentsProps {
+  hideVotes?: boolean;
+}
+
+export const DisclaveComments: React.VFC<DisclaveCommentsProps> = ({
+  hideVotes = false,
+}) => {
   const [url, setUrl] = useState<string | null>(null);
-  const iframeHeight = useIframeHeightListener("200");
+  const iframeHeight = useIframeHeightListener(hideVotes ? "200" : "250");
 
   useEffect(() => {
     const locationHref = window?.location?.href;
     if (!locationHref) return;
 
     try {
-      const iframeUrl = getIframeUrl(locationHref);
+      const iframeUrl = getIframeUrl(locationHref, {
+        hideVotes,
+      });
       setUrl(iframeUrl);
     } catch (e) {
       console.error(e);
