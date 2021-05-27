5f3e870144f3c0b75479c818c7487cdc4f95b968
JKrol <github@kroljakub.pl>
2021-05-27 10:57:34 +0200

    client side normalization update

--- client/src/modules/url/url.ts
+++ client/src/modules/url/url.ts
@@ -1,11 +1,17 @@
 import normalizeUrl from "normalize-url";
 
+const normalizationConfig: normalizeUrl.Options = {
+  defaultProtocol: "https:",
+  stripAuthentication: true,
+  stripTextFragment: true,
+};
+
 const urlPattern = new RegExp(
   "^(https?:\\/\\/)?" + // protocol
-  "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-  "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-  "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-  "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
     "(\\#[-a-z\\d_]*)?$",
   "i"
 ); // fragment locator
@@ -14,7 +20,8 @@ export const isUrl = (str: string | undefined): boolean => {
   return !!str && !!urlPattern.test(str);
 };
 
-export const stringToUrl = (str: string): string => normalizeUrl(str);
+export const stringToUrl = (str: string): string =>
+  normalizeUrl(str, normalizationConfig);
 
 export const encodeUrl = (url: string): string => {
   return encodeURIComponent(url).replace(
