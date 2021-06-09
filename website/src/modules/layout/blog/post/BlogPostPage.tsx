6ad3cb7a82de820a59aa0b431e63e4c1ae3a6372
JKrol <github@kroljakub.pl>
2021-06-09 12:37:35 +0200

    #193 use mdx for blog posts

--- website/src/modules/layout/blog/post/BlogPostPage.tsx
+++ website/src/modules/layout/blog/post/BlogPostPage.tsx
@@ -3,17 +3,26 @@ import { Layout } from '@/modules/layout';
 import { Post } from '@/modules/blog';
 import { DateTimePreview } from '@disclave/ui';
 import { DisclaveComments } from '@disclave/react-plugin';
-import styles from './BlogPostPage.module.css';
-import classNames from 'classnames';
 import { blogPostsImg } from '@/consts';
+import { MDXRemote } from 'next-mdx-remote';
+import Link from 'next/link';
+
+const components = {
+  Link: (props) => (
+    <Link href={props.href}>
+      <a className="text-primary font-semibold hover:underline">{props.children}</a>
+    </Link>
+  ),
+  h2: (props) => <h2 className="text-2xl" {...props} />,
+  p: (props) => <p className="my-5" {...props} />,
+  Image: (props) => <img {...props} />
+};
 
 export interface BlogPostPageProps {
   post: Post;
 }
 
 export const BlogPostPage: React.VFC<BlogPostPageProps> = ({ post }) => {
-  const contentClassName = classNames('my-6', styles.blogContent);
-
   return (
     <Layout>
       <section className="container mx-auto my-6 px-3">
@@ -27,10 +36,9 @@ export const BlogPostPage: React.VFC<BlogPostPageProps> = ({ post }) => {
             width={blogPostsImg.width}
             height={blogPostsImg.height}
           />
-          <div
-            className={contentClassName}
-            dangerouslySetInnerHTML={{ __html: post.contentHtml }}
-          />
+          <div className="my-6">
+            <MDXRemote {...post.mdxSource} components={components} />
+          </div>
           <div>
             <DisclaveComments />
           </div>
