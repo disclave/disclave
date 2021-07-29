064623d824168b0c76771bb742dbfcd7afd77eff
JKrol <github@kroljakub.pl>
2021-07-29 10:24:09 +0200

    custom server test configuration

---
+++ website/src/server.js
@@ -0,0 +1,17 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = parseInt(process.env.PORT) || 3000;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = req.url ? parse(req.url, true) : undefined;
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
